95f3d06636d20e883c39a52bd8830e276724c72c
Dhalo25 <leonardo@masinialeman.com>
2025-07-18 21:14:48 -0700

    Working server script added!!

---
+++ jirani-offline-lib-v2/server.js
@@ -0,0 +1,84 @@
+//Missing Safety Against Traversal Attacks
+//Fix default Title given to files
+
+import express from 'express'; //includes bodyParser module
+import fs from 'fs/promises'
+import multer from 'multer';
+import path from 'path';
+
+const app = express();
+const port = 3000; // For now
+
+//Get the absolute directory using server url location, instead of from where node was ran
+const __dirname = import.meta.dirname;
+console.log("A",__dirname);
+const uploadDir = path.join(__dirname, 'uploads'); //What will be used 
+const metadataDir = path.join(__dirname, 'metadata');
+
+console.log("B")
+
+//Setting Up Middleware 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('dist'));
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        const timestamp_str = new Date().toISOString().replace(/[.:]/g,'');
+        cb(null, timestamp_str + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+
+//Upload endpoint
+app.post('/upload', upload.single('file'), async (req, res) => {
+    if (!req.file)
+        return res.status(500).send({ message: 'No File Received' }); //Sending Http error
+
+    let metadataPath = '';
+    try{
+        const metadata = req.body.metadata? req.body.metadata: {};
+    
+        //Predefined
+        metadata.timestamp = new Date().toISOString();
+        metadata.mimetype = req.file.mimetype;
+        metadata.size = req.file.size;
+    
+        //Metadata Fields
+        metadata.title = 
+            req.body.title ? req.body.title : path.parse(req.file.filename).name
+        metadata.author = req.body.author
+        metadata.publisher = req.body.publisher
+        metadata.tags = req.body.tags
+        metadata.series = req.body.series
+        metadata.identifier = req.body.identifier
+    
+        metadataPath = path.join(metadataDir, `${req.file.filename}.json`) 
+        await fs.writeFile(metadataPath, JSON.stringify(metadata));
+
+        
+        res.status(201).send({
+            message: 'File upload successful!',
+            filename: req.file.filename
+        })
+    }
+    catch(err){
+        fs.unlink(metadataPath);
+        res.status(500).send({message: 'Failed upload'});
+    }
+})
+
+app.get('*', (req, res) => {
+    const indexPath = path.join(__dirname, 'index.html');
+    res.sendFile(indexPath);
+});
+
+app.listen(port, () => {
+    console.log('Server is running');
+})
--- jirani-offline-lib-v2/src/components/IndividualFormInput.tsx
+++ jirani-offline-lib-v2/src/components/IndividualFormInput.tsx
@@ -6,8 +6,7 @@ interface Props{
 }
 
 export default function IndividualFormInput({id, label, type = 'text', placeholderText=''}: Props) {
-    const helpTextId = `${id}Help`;
-
+    
     return ( // From Gemini:
     <div className="mb-3 text-start">
       <label htmlFor={id} className="form-label text-start fs-4">
